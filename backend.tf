12e74931653a50223a5cc833cc362ed0cf3fbbe1
Nima <nima_ziaee@yahoo.com>
2023-03-22 19:13:26 +0100

    Setup dynamodb locking

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "falconbirds3bucket"
-    key    = "build/airflow/terraform.tfstate"
-    region = "eu-north-1"
-  }
-}
--- provider.tf
+++ provider.tf
@@ -1,30 +1,23 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source  = "hashicorp/aws" 
+      version = "5.16.2"
     }
+  }
+  
+  # This backend configuration instructs Terraform to store its state in an S3 bucket.
+  backend "s3" {
+    bucket         = "falconbirds3bucket"  # Name of the S3 bucket where the state will be stored.
+    key            = "App10/terraform.tfstate" # Path within the bucket where the state will be read/written.
+    region         = "eu-north-1" # AWS region of the S3 bucket.
+    dynamodb_table = "terraform-lock" # DynamoDB table used for state locking.
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
   }
 }
 
-# Configure the AWS Provider
+# Configuration for the AWS provider.
 provider "aws" {
-  region     = var.region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region  = "eu-north-1"          # AWS region where resources will be provisioned.
+  profile = "nima"      # AWS CLI profile to use for authentication.
 }
-
-# # Configure the AWS Remote Backend - S3 Vucket
-# terraform {
-#   backend "s3" {
-#     bucket = "falconbird-bucket"
-#     key    = "build/airflow/terraform.tfstate"
-#     region = "eu-north-1"
-#   }
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
