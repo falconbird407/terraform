7e0cae4d69af11a5d2ef7cecff8886812a0b5368
nima <nima_ziaee@yahoo.com>
2024-03-09 14:23:46 -0500

    edit the ./github/workflows/terraform-apply.yaml

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region     = var.region
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
-  }
+}
 
 # # Configure the AWS Remote Backend - S3 Vucket
 # terraform {
--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
-variable "region" {}
-variable "instance_type" {}
-variable "ami_id" {}
-variable "key_name" {}
-variable "machine_name" {}
-variable "created_by" {}
 variable "AWS_ACCESS_KEY_ID" {
   default = ""
 }
 variable "AWS_SECRET_ACCESS_KEY" {
   default = ""
 }
-
+variable "region" {}
+variable "instance_type" {}
+variable "ami_id" {}
+variable "key_name" {}
+variable "machine_name" {}
+variable "created_by" {}
