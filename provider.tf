4dbd1905e170a73e46d77b8d87270a8efdae16dc
nima <nima_ziaee@yahoo.com>
2024-03-09 11:41:09 -0500

    Use our Terraform Cloud organization

--- provider.tf
+++ provider.tf
@@ -10,9 +10,9 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  }
 
 # # Configure the AWS Remote Backend - S3 Vucket
 # terraform {
--- variables.tf
+++ variables.tf
@@ -1,7 +1,5 @@
 variable "region" {}
 variable "instance_type" {}
-variable "access_key" {}
-variable "secret_key" {}
 variable "ami_id" {}
 variable "key_name" {}
 variable "machine_name" {}
